c2de518d460d9620a365bbff979c7d1816428fb3
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-01-03 14:37:51 +0200

    Feat: added profanity validation to PostModelSerialzier.

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from .models import Post
 
+from profanity_check import predict, predict_prob
+
+
 class PostModelSerializer(serializers.ModelSerializer):
     author = serializers.CharField(
         source="author.username",
         read_only=True
     )
+
+    def validate(self, data):
+        if predict([data["title"], data["description"]]).any():
+            raise serializers.ValidationError("Profanity is not allowed in the posts!")
+
+        return data
 
     class Meta:
         model = Post
