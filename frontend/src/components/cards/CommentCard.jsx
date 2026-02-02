870a895fd4b7dfff6d27a37d55e573a5540f40dd
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-02-02 15:02:11 +0200

    style: comment section and comment card

--- frontend/src/components/cards/CommentCard.jsx
+++ frontend/src/components/cards/CommentCard.jsx
@@ -1,14 +1,16 @@
+import dayjs from "dayjs";
 import "./CommentCard.css";
 
 const CommentCard = ({ comment }) => {
+  const formatedDate = dayjs(comment.created_at).format("HH:mm DD.MM.YYYY");
   return (
     <article className="comment-card">
-      <img src="/images/default-profile-img.jpeg" alt="profile-image"/>
+      <img src="/images/default-profile-img.jpeg" alt="profile-image" className="profile-image"/>
 
       <div className="comment-wrapper">
         <header className="comment-header">
           <h3 className="username">{comment.author.username}</h3>
-          <span className="date-created">{comment.created_at}</span>
+          <span className="date-created">{formatedDate}</span>
         </header>
 
         <p className="content">{comment.content}</p>
