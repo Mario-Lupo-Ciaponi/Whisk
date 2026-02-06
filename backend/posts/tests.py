b4b631e06973e0271fd51d7a281511e0dfc69908
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-02-06 19:28:33 +0200

    chore: added tests for TestPostListCreateAPIView

--- backend/posts/tests.py
+++ backend/posts/tests.py
@@ -1,5 +1,9 @@
+from io import BytesIO
+from PIL import Image
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework.serializers import ValidationError
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -10,6 +14,247 @@ User = get_user_model()
 
 
 class TestPostListCreateAPIView(APITestCase):
+    def setUp(self):
+        self.country = Country.objects.create(name="Bulgaria", code2="BG")
+        self.city = City.objects.create(name="Sofia", country=self.country)
+
+        self.user = User.objects.create_user(
+            username="TestUser", password="Test", country=self.country
+        )
+
+        image_io = BytesIO()
+        image = Image.new("RGB", (1, 1), "white")
+        image.save(image_io, "JPEG")
+        image_io.seek(0)
+
+        self.image_file = SimpleUploadedFile(
+            name="test-image.jpeg", content=image_io.read(), content_type="image/jpeg"
+        )
+
+        self.url = reverse("post-list")
+
+    def test__create_post_as_unauthenticated_user__returns_401(self):
+        data = {
+            "title": "A test post",
+            "description": "This is just a test post that has no real value outside this TestCase class",
+            "city_id": self.city.pk,
+            "image": self.image_file,
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertFalse(Post.objects.filter(pk=1).exists())
+
+    def test__create_post_with_valid_input__returns_201(self):
+        data = {
+            "title": "A test post",
+            "description": "This is just a test post that has no real value outside this TestCase class",
+            "city_id": self.city.pk,
+            "image": self.image_file,
+        }
+
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(self.url, data, format="multipart")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertTrue(Post.objects.filter(pk=1).exists())
+
+        post = Post.objects.get(pk=1)
+
+        self.assertEqual(post.title, data["title"])
+        self.assertEqual(post.description, data["description"])
+        self.assertEqual(post.city, self.city)
+        self.assertEqual(post.author, self.user)
+        self.assertTrue(post.image.url)
+
+    def test__create_post_profanity_validator_with_invalid_data__raises_validation_error(
+        self,
+    ):
+        data = {
+            "title": "A test fuck",
+            "description": "This is just a test post that has no real value outside this TestCase class",
+            "city_id": self.city.pk,
+            "image": self.image_file,
+        }
+
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(self.url, data, format="multipart")
+
+        data = response.data
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("title", data)
+        self.assertIn("profanity_error", data["title"])
+
+        self.assertFalse(Post.objects.filter(pk=1).exists())
+
+    def test__get_posts_with_no_posts__returns_pagination_list_with_empty_results(self):
+        response = self.client.get(self.url)
+        data = response.data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(data["count"], 0)
+        self.assertIsNone(data["next"])
+        self.assertIsNone(data["previous"])
+        self.assertEqual(len(data["results"]), 0)
+
+    def test__get_posts_with_posts__returns_pagination_list_with_results(self):
+        data = {
+            "title": "A test post",
+            "description": "This is just a test post that has no real value outside this TestCase class",
+            "city_id": self.city.pk,
+            "image": self.image_file,
+        }
+
+        Post.objects.create(
+            title="Lost Cat",
+            description="Lost it, please help me guys",
+            city=self.city,
+            author=self.user,
+            image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+        )
+
+        response = self.client.get(self.url)
+        data = response.data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(data["count"], 1)
+        self.assertIsNone(data["next"])
+        self.assertIsNone(data["previous"])
+        self.assertEqual(len(data["results"]), 1)
+
+    def test__get_posts_with_many_posts__returns_pagination_list_with_results(self):
+        posts_data = [
+            Post(
+                title=f"A test post {i + 1}",
+                description=f"This is just a test post that has no real value outside this TestCase class {i + 1}",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            )
+            for i in range(7)
+        ]
+
+        Post.objects.bulk_create(posts_data)
+
+        response = self.client.get(self.url)
+        data = response.data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(data["count"], 7)
+        self.assertIsNotNone(data["next"])
+        self.assertIsNone(data["previous"])
+        self.assertEqual(len(data["results"]), 6)
+
+    def test__get_posts_filter_with_found_query_params__returns_filtered_posts(self):
+        posts_data = [
+            Post(
+                title=f"A test post 1",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+            Post(
+                title=f"A test post 2",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                found=True,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+            Post(
+                title=f"A test post 3",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                found=True,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+        ]
+
+        Post.objects.bulk_create(posts_data)
+
+        response = self.client.get(f"{self.url}?found=True")
+        data = response.data
+
+        self.assertEqual(len(data["results"]), 2)
+
+    def test__get_posts_filter_with_city_query_params__returns_filtered_posts(self):
+        city_2 = City.objects.create(name="Vratsa", country=self.country)
+
+        posts_data = [
+            Post(
+                title=f"A test post 1",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=city_2,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+            Post(
+                title=f"A test post 2",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+            Post(
+                title=f"A test post 3",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+        ]
+
+        Post.objects.bulk_create(posts_data)
+
+        response = self.client.get(f"{self.url}?city={city_2.pk}")
+        data = response.data
+
+        self.assertEqual(len(data["results"]), 1)
+
+    def test__get_posts_filter_with_invalid_query_params__returns_all_posts(self):
+        posts_data = [
+            Post(
+                title=f"A test post 1",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+            Post(
+                title=f"A test post 2",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+            Post(
+                title=f"A test post 3",
+                description=f"This is just a test post that has no real value outside this TestCase class",
+                city=self.city,
+                author=self.user,
+                image="https://res.cloudinary.com/demo/image/upload/w_150,h_100,c_fill/sample.jpg",
+            ),
+        ]
+
+        Post.objects.bulk_create(posts_data)
+
+        response = self.client.get(f"{self.url}?description=dwad")
+        data = response.data
+
+        self.assertEqual(len(data["results"]), 3)
+
+
+class TestPostRetrieveUpdateDestroyAPIView(APITestCase):
     def setUp(self):
         self.country = Country.objects.create(name="Bulgaria", code2="BG")
         self.city = City.objects.create(name="Sofia", country=self.country)
@@ -81,6 +326,8 @@ class TestPostListCreateAPIView(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Post.objects.filter(pk=self.post.pk).exists())
+
+    from rest_framework.test import APIClient
 
     def test__updates_post_as_admin__updates_post(self):
         self.client.force_authenticate(self.super_user)
