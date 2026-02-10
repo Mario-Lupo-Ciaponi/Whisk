d2ed5dc5b9555ef0eb99fb764ebf2947291aaf8d
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-02-10 09:26:27 +0200

    feat: made the edit profile fields functional

--- frontend/src/components/forms/ProfileForm/ProfileForm.jsx
+++ frontend/src/components/forms/ProfileForm/ProfileForm.jsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import api from "../../../api/api.js";
 import "./ProfileForm.css";
 
 const ProfileForm = ({ currentUser, shouldNotEdit }) => {
   const [bio, setBio] = useState(currentUser.profile.bio);
-  const [profileType, setProfileType] = useState(
-    currentUser.profile.profile_type,
+  const [accountType, setAccountType] = useState(
+    currentUser.profile.account_type,
   );
+  const [selectedCity, setSelectedCity] = useState(currentUser.profile.city);
+  const [cities, setCities] = useState([]);
+
+  console.log(currentUser.profile.city);
+
+  const addCities = async () => {
+    const countryId = currentUser.country;
+
+    const response = await api.get("cities/", {
+      params: {
+        country: countryId,
+      },
+    });
+
+    setCities(response.data);
+  };
 
   const editProfile = async (event) => {
     event.preventDefault();
+    event.stopPropagation();
 
     try {
       const formData = new FormData();
 
-      formData.append("bio", bio);
+      formData.append("bio", bio.trim());
+      formData.append("account_type", accountType);
+      formData.append("city", selectedCity);
 
       await api.patch(`accounts/profile/${currentUser.id}/`, formData, {
         headers: {
@@ -29,11 +48,17 @@ const ProfileForm = ({ currentUser, shouldNotEdit }) => {
     }
   };
 
+  useEffect(() => {
+    if (currentUser?.country) addCities();
+  }, [currentUser]);
+
   return (
     <form className="profile-form">
       <div className="profile-fields">
         <div className="profile-field">
-          <label htmlFor="">Bio:</label>
+          <label className="profile-label" htmlFor="">
+            Bio:
+          </label>
           <input
             name="bio"
             className="profile-input bio"
@@ -48,14 +73,19 @@ const ProfileForm = ({ currentUser, shouldNotEdit }) => {
         </div>
 
         <div className="profile-field">
-          <label htmlFor="">Profile type:</label>
+          <label className="profile-label" htmlFor="">
+            Account type:
+          </label>
           <select
-            name="profile-type"
-            className="profile-type"
+            name="account-type"
+            className="profile-select type"
             disabled={shouldNotEdit}
-            value={profileType}
+            value={accountType}
+            onChange={(event) => {
+              setAccountType(event.target.value);
+            }}
           >
-            <option value="">Do not specify</option>
+            <option value={null}>Do not specify</option>
             <option value="pet owner">Pet owner</option>
             <option value="volunteer">Volunteer</option>
             <option value="shelter">Shelter</option>
@@ -63,14 +93,30 @@ const ProfileForm = ({ currentUser, shouldNotEdit }) => {
         </div>
 
         <div className="profile-field">
-          <label htmlFor="">City:</label>
-          <input
+          <label className="profile-label" htmlFor="">
+            City:
+          </label>
+          <select
             name="city"
-            className="profile-input city"
+            id="city"
+            className="profile-select city"
             disabled={shouldNotEdit}
-            type="text"
-            placeholder="Enter city"
-          />
+            value={selectedCity}
+            onChange={(event) => {
+              setSelectedCity(event.target.value);
+            }}
+          >
+            <option className="select-option" value={null}>
+              No city
+            </option>
+            {cities.map((city) => {
+              return (
+                <option className="select-option" value={city.id}>
+                  {city.name}
+                </option>
+              );
+            })}
+          </select>
         </div>
       </div>
       {!shouldNotEdit && (
