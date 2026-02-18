5a3721ef323b51199416f99018eb111460b89fef
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-02-18 15:28:49 +0200

    chore: changed the urls in common

--- backend/common/urls.py
+++ backend/common/urls.py
@@ -5,7 +5,16 @@ urlpatterns = [
     path("cities/", views.CityListAPIView.as_view(), name="cities"),
     path("countries/", views.CountryListAPIView.as_view(), name="countries"),
     path("contact/", views.ContactAPIView.as_view(), name="contact"),
-    path("notifications/", include([
-        path("", views.GetUnreadNotificationsAPIView.as_view(), name="unread-notifications"),
-    ])),
+    path(
+        "notifications/",
+        include(
+            [
+                path(
+                    "",
+                    views.GetUnreadNotificationsAPIView.as_view(),
+                    name="unread-notifications",
+                ),
+            ]
+        ),
+    ),
 ]
