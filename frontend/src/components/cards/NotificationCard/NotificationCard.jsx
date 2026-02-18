c09ffae00a962034f79cfbb0c766b7d830baf9bf
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-02-18 18:55:25 +0200

    chore: improoved the notification card UX

--- frontend/src/components/cards/NotificationCard/NotificationCard.jsx
+++ frontend/src/components/cards/NotificationCard/NotificationCard.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router";
 import "./NotificationCard.css";
+import {useEffect} from "react";
 
 const NotificationCard = ({ notification }) => {
+  useEffect(() => {
+    console.log(notification)
+  }, []);
   return (
     <article className="notification-card">
-      <p>{notification.text}</p>
+      <p>
+        {
+          notification.sender ? <Link className="link" to={`/profile/${notification.sender.id}`}>{notification.sender.username}</Link>
+            :
+            "Anonymous user"} {notification.text} <Link className="link" to={`/post/${notification.post_id}`}>Post</Link>
+      </p>
     </article>
   );
 };
