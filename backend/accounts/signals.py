5e0e646c23a7bd82484dac38e0c530ed48b08fac
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-02-19 17:41:03 +0200

    docs: added doc string in the signals

--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -9,6 +9,20 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a Profile for a newly created User and send a welcome email.
+
+    This receiver runs after a User is saved. If the User was just created,
+    it will:
+      - create an associated Profile instance,
+      - send a welcoming email to the new user.
+
+    Args:
+        sender: The model class (User).
+        instance: The saved User instance.
+        created (bool): True if the instance was created (not updated).
+        **kwargs: Additional keyword arguments provided by Django signals.
+    """
     if created:
         Profile.objects.create(user=instance)
 
--- backend/common/signals.py
+++ backend/common/signals.py
@@ -1,3 +1,12 @@
+"""
+Signal handlers for creating Notification records and sending notification emails.
+
+This module defines Django signal receivers that react to the creation of
+PetLocation and Comment instances. When a new location or comment is created
+by a user other than the post author, a Notification is created for the post
+author and a notification email is sent asynchronously.
+"""
+
 import threading
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
@@ -13,6 +22,19 @@ from .mails import send_location_added_mail, send_comment_added_mail
 
 @receiver(post_save, sender=PetLocation)
 def send_location_notification_to_user(sender, instance, created, **kwargs):
+    """
+    Handle post-save signals for PetLocation.
+
+    If a PetLocation was just created and its author is not the author of the
+    related post, create a Notification for the post author and send an email
+    to inform them that a new location was added.
+
+    Args:
+        sender: The model class (PetLocation).
+        instance: The saved PetLocation instance.
+        created (bool): True if the instance was created (not updated).
+        **kwargs: Additional keyword arguments passed by Django signals.
+    """
     if created:
         post = instance.post
         post_author = instance.post.author
@@ -33,6 +55,19 @@ def send_location_notification_to_user(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Comment)
 def send_comment_notification_to_user(sender, instance, created, **kwargs):
+    """
+    Handle post-save signals for Comment.
+
+    If a Comment was just created and its author is not the author of the
+    related post, create a Notification for the post author and send an email
+    to inform them that a new comment was added.
+
+    Args:
+        sender: The model class (Comment).
+        instance: The saved Comment instance.
+        created (bool): True if the instance was created (not updated).
+        **kwargs: Additional keyword arguments passed by Django signals.
+    """
     if created:
         post = instance.post
         post_author = instance.post.author
