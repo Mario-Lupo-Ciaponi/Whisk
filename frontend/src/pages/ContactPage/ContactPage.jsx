8a4410a354bbbd9347ce25bc84fb87faccc4dbdc
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-03-16 19:33:43 +0200

    chore: added og meta tags in contact page

--- frontend/src/pages/ContactPage/ContactPage.jsx
+++ frontend/src/pages/ContactPage/ContactPage.jsx
@@ -1,20 +1,31 @@
 import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router";
+import { Helmet } from "react-helmet-async";
 import ContactForm from "../../components/forms/ContactForm/ContactForm.jsx";
 import "./ContactPage.css";
 
-const ContactPage = ({ currentUser }) => {
+const ContactPage = ({ currentUser, baseUrl }) => {
   const { t } = useTranslation();
+  const location = useLocation();
+
+  const pageTitle = t("contact.title");
+  const pageUrl = `${baseUrl}/${location.pathname}`;
 
   return (
-    <div className="contact-container">
-      <title>{t("contact.title")}</title>
+    <>
+      <Helmet>
+        <title>{pageTitle}</title>
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:url" content={pageUrl} />
+      </Helmet>
+      <div className="contact-container">
+        <header className="contact-header">
+          <h1 className="contact-title">{t("contact.title")}</h1>
+        </header>
 
-      <header className="contact-header">
-        <h1 className="contact-title">{t("contact.title")}</h1>
-      </header>
-
-      <ContactForm currentUser={currentUser} />
-    </div>
+        <ContactForm currentUser={currentUser} />
+      </div>
+    </>
   );
 };
 
